8bc6e20adbb26d9abee75ef3f67eaaf89bdee4f7
Merritt Adkins <MerrittE@protonmail.com>
2017-09-30 16:22:12 -1000

    Adding messages on errors Added previous code outlined in lesson 4.8

--- MessageApp/MessagesViewController.swift
+++ MessageApp/MessagesViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 import Firebase
-import JSQMessagesViewController
+import JSQMessagesViewController // can't get this to load right
+
 
 class MessagesViewController: JSQMessagesViewController {
     
@@ -45,18 +46,118 @@ class MessagesViewController: JSQMessagesViewController {
         senderId = AuthenticationManager.sharedInstance.userId
         senderDisplayName = AuthenticationManager.sharedInstance.userName
         
-        collectionView!
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+        
+        setupMessageBubbles()
+        
+        ref = Database.database().reference()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
     }
-    */
-
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item]
+        if message.senderId == senderId {
+            return outgoingMessageBubbleImage
+        } else {
+            return incomingMessageBubbleImage
+        }
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        
+        let message = messages[indexPath.item]
+        
+        if message.senderId == senderId {
+            cell.textView!.textColor = .white
+        } else {
+            cell.textView!.textColor = .black
+        }
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
+        let message = messages[indexPath.item]
+        switch message.senderId {
+        //Here we are displaying everyones name above their message except for the "Senders"
+        case senderId:
+            return nil
+        default:
+            guard let senderDisplayName = message.senderDisplayName else {
+                assertionFailure()
+                return nil
+            }
+            return NSAttributedString(string: senderDisplayName)
+            
+        }
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
+        return 20.0
+    }
+    
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        let messageRef = ref.child(MessageProperties.messageType).childByAutoId()
+        let message = [
+            MessageProperties.text: text!,
+            MessageProperties.senderID: senderId!,
+            MessageProperties.senderDisplayName: senderDisplayName!
+        ]
+        messageRef.setValue(message)
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound()
+        
+        finishSendingMessage()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        messages.removeAll()
+        databaseHandle = ref.child(MessageProperties.messageType).observe(.childAdded, with: { (snapshot) -> Void in
+            if let value = snapshot.value as? [String:AnyObject] {
+                let id = value[MessageProperties.senderID] as! String
+                let text = value[MessageProperties.text] as! String
+                let name = value[MessageProperties.senderDisplayName] as! String
+                
+                self.addMessage(id: id, text: text, name: name)
+                self.finishReceivingMessage()
+            }
+        })
+    }
+    
+    private func setupMessageBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingMessageBubbleImage = factory?.outgoingMessagesBubbleImage(
+            with: UIColor(red:0.97, green:0.57, blue:0.01, alpha:1.00))
+        incomingMessageBubbleImage = factory?.incomingMessagesBubbleImage(
+            with: .jsq_messageBubbleLightGray())
+    }
+    
+    func addMessage(id: String, text: String, name: String) {
+        let message = JSQMessage(senderId: id, displayName: name, text: text)
+        messages.append(message!)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.ref.removeObserver(withHandle: databaseHandle)
+    }
+    
+    @IBAction func signOut(sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            AuthenticationManager.sharedInstance.loggedIn = false
+            dismiss(animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print ("Error signing out: \(signOutError)")
+        }
+    }
 }
